300d4485d8f71abf526c30b2ffb237ea2513fd43
Monica Del Rio <mlisethdelrio@gmail.com>
2020-02-26 10:30:04 -0600

    cambio de ruta

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", e=>{
       event.preventDefault();
       
      let nombre = document.querySelector("txtNombre").value; 
-     fetch ('http://localhost:8080/programacionva1/saludo.php?nombre=$'(nombre))
+     fetch ('saludo.php?nombre=$'(nombre))
      .then(resp=>resp.text())
      .then(respuesta=>{
        document.querySelector("#lblRespuesta").innerHTML = respuesta;
