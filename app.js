1550bbb930550aeaa0eb017826585e145cf5c567
Monica Del Rio <mlisethdelrio@gmail.com>
2020-02-26 10:32:55 -0600

    saludo con ajax

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", e=>{
-  const form = document.querySelector("#frmSaludo");
+  const form = document.querySelector("#frm");
   form.addEventListener("submit", event=>{
       event.preventDefault();
       
      let nombre = document.querySelector("txtNombre").value; 
-     fetch ('saludo.php?nombre=$'(nombre))
+     fetch ('saludo.php?nombre=$'(Nombre))
      .then(resp=>resp.text())
      .then(respuesta=>{
        document.querySelector("#lblRespuesta").innerHTML = respuesta;
--- saludo.php
+++ saludo.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/El_Salvador');
 
-if( $_GET && isset ($_GET['nombre'])){
+if( $_GET && isset ($_GET['Nombre'])){
 $nombre = $_GET['nombre'];
 echo 'Hola'.$nombre. 'desde el servidor'.date('d-m-Y H:i:s');
 }else{
