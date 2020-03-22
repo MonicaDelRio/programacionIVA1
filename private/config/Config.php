34b152841c1bbe25da541f263f8882b0b3129352
Monica Del Rio <mlisethdelrio@gmail.com>
2020-03-21 19:16:41 -0600

    cambios realizados :3

--- private/config/Config.php
+++ private/config/Config.php
@@ -1,7 +1,7 @@
 <?php
 
-include('../conexion/DB.php');
-$conexion = new conexion('localhost','root','','db_academica');
+include('../../conexion/DB.php');
+$conexion = new conexion('127.0.0.1:5501','root','','db_app_academica');
 
 
 ?>
