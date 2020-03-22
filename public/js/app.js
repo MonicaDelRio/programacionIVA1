c42f5dbd7709c38658a36bcb0b9f74af0a2454cd
Monica Del Rio <mlisethdelrio@gmail.com>
2020-03-21 22:32:42 -0600

    cambios docente

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,11 @@
 
 var $ = el => document.querySelector(el);
 document.addEventListener("DOMContentLoaded",event=>{
-    let mostrarVista = $("[class*='mostrar']");
-    mostrarVista.addEventListener('click',e=>{
-        e.stopPropagation();
+    let mostrarVista = $("[class*='mostrar']"),
+    mostrardocentes=$("[class*='mostrardocentes']");
 
+    mostrarVista.addEventListener('click',e=>{ 
+        e.stopPropagation();
         let modulo = e.toElement.dataset.modulo;
         fetch('public/vistas/alumnos/alumnos.html').then( resp=>resp.text() ).then(resp=>{
             $(`#vista-${modulo}`).innerHTML = resp;
@@ -16,8 +17,27 @@ document.addEventListener("DOMContentLoaded",event=>{
 
             let cuerpo = $("body"),
                 script = document.createElement("script");
-            script.src = `public/vistas/${modulo}/${modulo}.js`;
+            script.src = `public/vistas/alumnos/alumnos.js/${modulo}.js`;
             cuerpo.appendChild(script);
         });
     });
+        mostrardocentes.addEventListener('click',e=>{
+            e.stopPropagation();
+            let modulo = e.toElement.dataset.modulo;
+            console.log(modulo);
+            fetch('public/vistas/docentes/docentes.html').then(resp=>resp.text() ).then(resp=>{
+                $(`#vista-${modulo}`).innerHTML=resp;
+
+                let btnCerrar=$(".close");
+                btnCerrar.addEventListener("click",event=>{
+                    $(`#vista-${modulo}`).innerHTML="";
+                });
+
+                let cuerpo =$("body"),
+                script=document.createElement("script");
+                script.src =`public/vistas/docentes/docentes.js/${modulo}.js`;
+                cuerpo.appendChild(script);
+            });
+        });
 });
+
